f13d31b86b699f73cadefcb7727178536bffdd9e
Lia Neville <lianeville5@gmail.com>
2024-04-05 11:39:47 -0400

    lid has min and max rotation values

--- src/App.jsx
+++ src/App.jsx
@@ -1,66 +1,6 @@
 import { Canvas, useFrame } from "@react-three/fiber/"
-import { useRef, useState, useEffect, useMemo } from "react"
 import "./App.css"
-import { OrbitControls, useHelper } from "@react-three/drei"
-import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
-import { useGesture } from "@use-gesture/react"
-
-function TreasureChest() {
-	const loader = new GLTFLoader()
-	const chestLidRef = useRef(null)
-	const chestBaseRef = useRef(null)
-	const [scene, setScene] = useState(null)
-	const controlsRef = useRef(null)
-
-	useEffect(() => {
-		loader.load("/src/assets/3D-Models/Chest_Separated.glb", gltf => {
-			const chestGroup = gltf.scene.children[0]
-
-			const lidMesh = chestGroup.children.find(
-				child => child.name === "Chest_Lid"
-			)
-			const baseMesh = chestGroup.children.find(
-				child => child.name === "Chest_Base"
-			)
-
-			chestLidRef.current = lidMesh
-			console.log(chestLidRef.current)
-			chestBaseRef.current = baseMesh
-
-			setScene(gltf.scene)
-		})
-	}, [])
-
-	const handleDragEnd = () => {
-		controlsRef.current.enableRotate = true
-	}
-
-	const handleDrag = state => {
-		controlsRef.current.enableRotate = false
-		// Check if chestLidRef is defined before accessing its properties
-		if (chestLidRef.current) {
-			chestLidRef.current.rotation.x += state.delta[0] * 0.01 // Adjust sensitivity
-		}
-	}
-
-	const bind = useGesture({ onDrag: handleDrag, onDragEnd: handleDragEnd })
-
-	return (
-		<>
-			{scene && (
-				<>
-					<primitive object={chestBaseRef.current} />
-					<primitive {...bind()} object={chestLidRef.current} />
-					<OrbitControls
-						ref={controlsRef}
-						enableZoom={false}
-						enablePan={false}
-					/>
-				</>
-			)}
-		</>
-	)
-}
+import TreasureChest from "./Components/Boxes/TreasureChest"
 
 function Scene() {
 	// const directionalLightRef = useRef()
---
+++ src/Components/Boxes/TreasureChest.jsx
@@ -0,0 +1,72 @@
+import { useRef, useState, useEffect, useMemo } from "react"
+import { Canvas, useFrame } from "@react-three/fiber/"
+
+import { OrbitControls, useHelper } from "@react-three/drei"
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader"
+import { useGesture } from "@use-gesture/react"
+
+function TreasureChest() {
+	const loader = new GLTFLoader()
+	const chestLidRef = useRef(null)
+	const chestBaseRef = useRef(null)
+	const [scene, setScene] = useState(null)
+	const controlsRef = useRef(null)
+
+	useEffect(() => {
+		loader.load("/src/assets/3D-Models/Chest_Separated.glb", gltf => {
+			const chestGroup = gltf.scene.children[0]
+
+			const lidMesh = chestGroup.children.find(
+				child => child.name === "Chest_Lid"
+			)
+			const baseMesh = chestGroup.children.find(
+				child => child.name === "Chest_Base"
+			)
+
+			chestLidRef.current = lidMesh
+			console.log(chestLidRef.current.rotation.x)
+			chestBaseRef.current = baseMesh
+
+			setScene(gltf.scene)
+		})
+	}, [])
+
+	const handleDragEnd = () => {
+		controlsRef.current.enableRotate = true
+	}
+
+	const handleDrag = state => {
+		controlsRef.current.enableRotate = false
+
+		if (chestLidRef.current) {
+			const min = -1
+			const max = 0.95
+			const lid = chestLidRef.current.rotation
+			const rotation = state.delta[0] * 0.01
+
+			const newRotationX = Math.min(Math.max(lid.x + rotation, min), max)
+
+			lid.x = newRotationX
+		}
+	}
+
+	const bind = useGesture({ onDrag: handleDrag, onDragEnd: handleDragEnd })
+
+	return (
+		<>
+			{scene && (
+				<>
+					<primitive object={chestBaseRef.current} />
+					<primitive {...bind()} object={chestLidRef.current} />
+					<OrbitControls
+						ref={controlsRef}
+						// enableZoom={false}
+						// enablePan={false}
+					/>
+				</>
+			)}
+		</>
+	)
+}
+
+export default TreasureChest
