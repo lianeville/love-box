c9984a6a700abe8e7a82c15224ba3d3fa15fae5d
Lia Neville <lianeville5@gmail.com>
2024-04-13 15:40:51 -0400

    added tailwind

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -4,19 +4,23 @@ import { create } from "zustand"
 const dbHost = import.meta.env.VITE_DB_HOST
 
 const useBoxStore = create(set => ({
-	activeBox: null,
+	activeBox: 2,
 	inc: () => set(state => ({ activeBox: 5 })),
 }))
 
 function ChestHi() {
 	const { activeBox, loadBox } = useBoxStore()
 
-	return <button onClick={loadBox}>{activeBox}</button>
+	return (
+		<button className="h-6 w-6 bg-gray-200 text-black" onClick={loadBox}>
+			{activeBox}
+		</button>
+	)
 }
 
 function App() {
 	return (
-		<div>
+		<div className="h-full flex">
 			<ChestHi></ChestHi>
 			<ModelViewer />
 		</div>
