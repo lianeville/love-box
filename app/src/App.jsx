c0aa32b79ca18881edc5bceb7d1b5914d964fd94
Lia Neville <lianeville5@gmail.com>
2024-04-13 15:34:12 -0400

    progress on state management

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,23 @@
 import "./App.css"
 import ModelViewer from "./ModelViews"
+import { create } from "zustand"
+const dbHost = import.meta.env.VITE_DB_HOST
+
+const useBoxStore = create(set => ({
+	activeBox: null,
+	inc: () => set(state => ({ activeBox: 5 })),
+}))
+
+function ChestHi() {
+	const { activeBox, loadBox } = useBoxStore()
+
+	return <button onClick={loadBox}>{activeBox}</button>
+}
 
 function App() {
 	return (
 		<div>
+			<ChestHi></ChestHi>
 			<ModelViewer />
 		</div>
 	)
